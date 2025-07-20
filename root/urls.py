a800d0167fbfcd150cac33f55b5f839d080d869b
grynevych89 <grynevych89@gmail.com>
2025-07-20 18:16:42 +0200

    update static

--- root/urls.py
+++ root/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('games.urls')),
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
