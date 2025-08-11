3db14a90f1d8637afcbf7bc9a12bf95fc61cfbdd
grynevych89 <grynevych89@gmail.com>
2025-08-11 14:14:22 +0200

    update urls

--- root/urls.py
+++ root/urls.py
@@ -1,14 +1,15 @@
 from django.conf import settings
-from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from products.admin import custom_admin_site
+from django.shortcuts import redirect
+
 
 urlpatterns = [
     path('admin/', custom_admin_site.urls),
     path('tinymce/', include('tinymce.urls')),
     path('api/', include('products.api.urls_api')),
-
+    path('', lambda request: redirect('/api/', permanent=False)),
 ]
 
 if settings.DEBUG:
